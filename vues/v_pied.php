705f1ea0414bdaac89bccae3853c95bd4a8c6f30
tsivya <tsivya@192.168.0.21>
2021-03-08 22:42:55 +0100

    Version 1

---
+++ vues/v_pied.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Vue Pied de page
+ *
+ * PHP Version 7
+*
+* @category  PPE
+* @package   GSB
+* @author   Tsivya Suissa
+* @author    Beth Sefer
+*/
+?>
+        </div>
+    </body>
+</html>
+
+
